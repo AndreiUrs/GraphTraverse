dceecda43a12ffbd5c2b67f213c6b2febc552a24
AndreiUrs <ursachi.andrei1991@gmail.com>
2019-08-22 08:54:15 +0200

    Unit Testing added

---
+++ GraphTraverseTest/GraphTraverseTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using GraphTraverse;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphTraverseTest
+{
+    [TestClass]
+    public class GraphTraverseTest
+    {
+        private readonly string testGraph = ConfigurationManager.AppSettings["testGraph"];
+      
+        public GraphTraverseTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GenerateGraph()
+        {
+          var gb = new GraphGenerator();
+          var  myGrapth = gb.GenerateGraph(testGraph);
+
+          Assert.IsNotNull(myGrapth);
+          Assert.IsTrue(myGrapth.Count > 0);
+
+        }
+
+        [TestMethod]
+        public void MaxSum()
+        {
+            const int expectedSum = 16;
+
+            var gb = new GraphGenerator();
+            var fistNode = gb.GenerateGraph(testGraph).FirstOrDefault();
+         
+            var calculatedSum = fistNode.GetMaxSum();
+
+            Assert.AreEqual(expectedSum, calculatedSum);
+        }
+
+        [TestMethod]
+        public void BestPath()
+        {
+            List<int> expectedPath = new List<int> { 1, 8, 5, 2 };
+
+            var gb = new GraphGenerator();
+            var firstNode = gb.GenerateGraph(testGraph).FirstOrDefault();
+
+            //we must calculate sum before we get path
+            firstNode.GetMaxSum();
+
+            var calculatedPath = firstNode.GetBestPath();
+
+            for (int i = 0; i < expectedPath.Count; i++)
+            {
+                Assert.AreEqual(expectedPath[i], calculatedPath[i]);
+            }
+
+        }
+
+    }
+}
---
+++ GraphTraverseTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GraphTraverseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphTraverseTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67a4a36b-eb0e-478c-a9fe-768255439f10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
